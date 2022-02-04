c6c6c585f0e96c4c0e87997bb13d00e2d4e053f8
Nikolay Nikolaenko <niko.dr@gmail.com>
2022-02-04 13:25:00 +0200

    adds def

--- customers/read_customers.py
+++ customers/read_customers.py
@@ -1,5 +1,13 @@
-CUSTOMERS_FILE = "customers.csv"
+CUSTOMERS_FILE = "customers/customers.csv"
 
+# function for separate the string by ";"
+def separate_string(string):
+    lst = string.split(";")
+    return lst
+
+# Open the file for reading
+f  = open(CUSTOMERS_FILE, "r")
+print( separate_string(f.readline()) )
 # Loop customers.csv 
 
 # find by name
